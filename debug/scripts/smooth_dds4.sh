b5a9a83c6087b3576c21f99921ca9086508934c8
Joshua Albert <albert@strw.leidenuniv.nl>
2019-10-17 01:41:40 +0200

    scripts for pipeline. Still debugging.

---
+++ debug/scripts/smooth_dds4.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# usage function
+function usage()
+{
+   cat << HEREDOC
+
+   Usage: $progname --script_dir --working_dir --data_dir --obs_num --conda_env
+
+   optional arguments:
+     -h, --help           show this help message and exit
+
+HEREDOC
+}
+
+# initialize variables
+progname=$(basename $0)
+conda_env="tf_py"
+script_dir=
+obs_num=
+data_dir=
+working_dir=
+
+# use getopt and store the output into $OPTS
+# note the use of -o for the short options, --long for the long name options
+# and a : for any option that takes a parameter
+OPTS=$(getopt -o "h" --long "help,conda_env:,script_dir:,obs_num:,data_dir:,working_dir:" -n "$progname" -- "$@")
+if [ $? != 0 ] ; then echo "Error in command line arguments." >&2 ; usage; exit 1 ; fi
+eval set -- "$OPTS"
+
+while true; do
+  # uncomment the next line to see how shift is working
+  # echo "\$1:\"$1\" \$2:\"$2\""
+  case "$1" in
+    -h | --help ) usage; exit; ;;
+    --conda_env ) conda_env="$2"; shift 2 ;;
+    --script_dir ) script_dir="$2"; shift 2 ;;
+    --working_dir ) working_dir="$2"; shift 2 ;;
+    --data_dir ) data_dir="$2"; shift 2 ;;
+    --obs_num ) obs_num="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+source ~/.bashrc
+source activate $conda_env
+cmd = "$script_dir/smooth_dds4.py --obs_num=$obs_num --data_dir=$data_dir --working_dir=$working_dir"
+eval $cmd
+source deactivate $conda_env
