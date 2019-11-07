2f52af4fbb0bd46803b853960a6f010d5bfcdc6d
Joshua Albert <albert@strw.leidenuniv.nl>
2019-11-07 01:36:50 +0100

    search optimise possible

--- bayes_gain_screens/outlier_detection.py
+++ bayes_gain_screens/outlier_detection.py
@@ -128,7 +128,7 @@ def filter_tec_dir(y,  directions, init_y_uncert=None, min_res=8.,  **kwargs):
         for a in range(Na):
             keep = np.logical_not(time_flag[:,a,t])#np.ones(Nd, dtype=np.bool)
             for i in range(maxiter):
-                svm = Rbf(*list(X[keep, :].T), y[keep, a, t], smooth=0.2, **kwargs)
+                svm = Rbf(*list(X[keep, :].T), y[keep, a, t], smooth=0.3, **kwargs)
                 y_star = svm(*x_list0)
                 dy = np.abs(y_star - y[:, a, t])
                 # print(np.median(dy), np.percentile(dy, 95))
