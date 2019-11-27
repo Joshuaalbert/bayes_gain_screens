ff6bd16564ae595ea76f54d46417c6e9a67b01a2
Joshua Albert <albert@strw.leidenuniv.nl>
2019-11-27 18:35:49 +0100

    some doc strings

--- debug/scripts/pipeline.py
+++ debug/scripts/pipeline.py
@@ -33,7 +33,7 @@ class CMD(object):
 
     def __call__(self):
         proc_log = os.path.join(self.working_dir, 'state.log')
-        cmd = ' \\\n\t'.join(self.cmd+['2>&1 | tee -a {}'.format(proc_log)])
+        cmd = ' \\\n\t'.join(self.cmd + ['2>&1 | tee -a {}'.format(proc_log)])
         print("Running:\n{}".format(cmd))
         exit_status = subprocess.call(cmd, shell=True)
         print("Finisihed:\n{}\nwith exit code {}".format(cmd, exit_status))
@@ -71,6 +71,19 @@ def make_working_dir(root_working_dir, name, do_flag):
 
 
 def iterative_topological_sort(graph, start):
+    """
+    Get Depth-first topology.
+
+    :param graph: dependency dict (like a dask)
+        {'a':['b','c'],
+        'c':['b'],
+        'b':[]}
+    :param start: str
+        the node you want to search from.
+        This is equivalent to the node you want to compute.
+    :return: list of str
+        The order get from `start` to all ancestors in DFS.
+    """
     seen = set()
     stack = []  # path variable is gone, stack and order are new
     order = []  # order will be in reverse order at first
@@ -87,10 +100,24 @@ def iterative_topological_sort(graph, start):
 
     return stack + order[::-1]  # new return value!
 
+
 def now():
     return datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
+
 def execute_dask(dsk, key, timing_file=None, state_file=None):
+    """
+    Go through the dask in topo order using DFS to reach `key`
+    :param dsk: dict
+        Dask graph
+    :param key: str
+        The node you want to arrive at.
+    :param timing_file: str
+        Where to store timing info
+    :param state_file:
+        Where to store pipeline state
+    :return:
+    """
     graph = {k: v[1:] for k, v in dsk.items()}
     topo_order = iterative_topological_sort(graph, key)[::-1]
     res = {}
@@ -272,15 +299,27 @@ def main(archive_dir, root_working_dir, script_dir, obs_num, region_file, ncpu, 
     else:
         dsk['smooth_dds4'] = (lambda *x: None, 'solve_dds4')
 
+    if do_tec_inference:
+        cmd = CMD(tec_inference_working_dir, script_dir, 'tec_inference.sh', 'bash')
+        cmd.add('conda_env', 'tf_py')
+        cmd.add('script_dir', script_dir)
+        cmd.add('obs_num', obs_num)
+        cmd.add('data_dir', subtract_working_dir)
+        cmd.add('working_dir', tec_inference_working_dir)
+        cmd.add('ref_dir', ref_dir)
+        dsk['tec_inference'] = (cmd, 'smooth_dds4', 'solve_dds4')
+    else:
+        dsk['tec_inference'] = (lambda *x: None, 'smooth_dds4', 'solve_dds4')
+
     if do_slow_dds4:
         cmd = CMD(slow_dds4_working_dir, script_dir, 'slow_solve_on_subtracted.py')
         cmd.add('ncpu', ncpu)
         cmd.add('obs_num', obs_num)
         cmd.add('data_dir', subtract_working_dir)
         cmd.add('working_dir', slow_dds4_working_dir)
-        dsk['slow_solve_dds4'] = (cmd, 'smooth_dds4')
+        dsk['slow_solve_dds4'] = (cmd, 'tec_inference')
     else:
-        dsk['slow_solve_dds4'] = (lambda *x: None, 'smooth_dds4')
+        dsk['slow_solve_dds4'] = (lambda *x: None, 'tec_inference')
 
     if do_image_smooth:
         cmd = CMD(image_smooth_working_dir, script_dir, 'image.py')
@@ -291,9 +330,9 @@ def main(archive_dir, root_working_dir, script_dir, obs_num, region_file, ncpu, 
         cmd.add('working_dir', image_smooth_working_dir)
         cmd.add('script_dir', script_dir)
         cmd.add('use_init_dico', True)
-        dsk['image_smooth'] = (cmd, 'smooth_dds4')
+        dsk['image_smooth'] = (cmd, 'tec_inference')
     else:
-        dsk['image_smooth'] = (lambda *x: None, 'smooth_dds4')
+        dsk['image_smooth'] = (lambda *x: None, 'tec_inference')
 
     if do_image_dds4:
         cmd = CMD(image_smooth_working_dir, script_dir, 'image.py')
@@ -307,18 +346,6 @@ def main(archive_dir, root_working_dir, script_dir, obs_num, region_file, ncpu, 
         dsk['image_dds4'] = (cmd, 'solve_dds4')
     else:
         dsk['image_dds4'] = (lambda *x: None, 'solve_dds4')
-
-    if do_tec_inference:
-        cmd = CMD(tec_inference_working_dir, script_dir, 'tec_inference.sh', 'bash')
-        cmd.add('conda_env', 'tf_py')
-        cmd.add('script_dir', script_dir)
-        cmd.add('obs_num', obs_num)
-        cmd.add('data_dir', subtract_working_dir)
-        cmd.add('working_dir', tec_inference_working_dir)
-        cmd.add('ref_dir', ref_dir)
-        dsk['tec_inference'] = (cmd, 'smooth_dds4', 'solve_dds4')
-    else:
-        dsk['tec_inference'] = (lambda *x: None, 'smooth_dds4', 'solve_dds4')
 
     if do_infer_screen:
         cmd = CMD(infer_screen_working_dir, script_dir, 'infer_screen.sh', 'bash')
@@ -381,9 +408,9 @@ def main(archive_dir, root_working_dir, script_dir, obs_num, region_file, ncpu, 
         cmd.add('working_dir', image_smooth_slow_working_dir)
         cmd.add('script_dir', script_dir)
         cmd.add('use_init_dico', True)
-        dsk['image_smooth_slow'] = (cmd, 'smooth_dds4', 'slow_solve_dds4', 'merge_slow')
+        dsk['image_smooth_slow'] = (cmd, 'tec_inference', 'slow_solve_dds4', 'merge_slow')
     else:
-        dsk['image_smooth_slow'] = (lambda *x: None, 'smooth_dds4', 'slow_solve_dds4', 'merge_slow')
+        dsk['image_smooth_slow'] = (lambda *x: None, 'tec_inference', 'slow_solve_dds4', 'merge_slow')
 
     dsk['endpoint'] = (lambda *x: None,) + tuple([k for k in dsk.keys()])
     # state_file = os.path.join(root_working_dir, 'STATE_{:03d}'.format(len(glob.glob(os.path.join(root_working_dir, 'STATE_*')))))
@@ -446,9 +473,9 @@ def add_args(parser):
 
 def test_main():
     main('/home/albert/store/lockman/archive',  # P126+65',
-         '/home/albert/store/root_nominal',
+         '/home/albert/store/root_redo',
          '/home/albert/store/scripts',
-         obs_num=667218,  # 664320,#667204,#664480,#562061,
+         obs_num=342938,  # 664320,#667204,#664480,#562061,
          region_file='/home/albert/store/lockman/LHdeepbright.reg',
          ref_image_fits=None,  # '/home/albert/store/lockman/lotss_archive_deep_image.app.restored.fits',
          ncpu=32,
@@ -457,14 +484,14 @@ def test_main():
          deployment_type='directional',
          no_subtract=False,
          do_choose_calibrators=0,
-         do_subtract=2,
-         do_image_subtract_dirty=2,
-         do_solve_dds4=2,
+         do_subtract=0,
+         do_image_subtract_dirty=0,
+         do_solve_dds4=0,
          do_smooth_dds4=2,
-         do_slow_dds4=2,
+         do_slow_dds4=0,
          do_tec_inference=2,
-         do_merge_slow=2,
-         do_infer_screen=0,
+         do_merge_slow=0,
+         do_infer_screen=2,
          do_image_dds4=0,
          do_image_smooth=0,
          do_image_smooth_slow=0,
