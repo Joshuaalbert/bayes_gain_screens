f994bbc5055d6626bca7ac0b936803c3eaefb96b
Joshua Albert <albert@strw.leidenuniv.nl>
2019-11-27 01:54:38 +0100

    Set improved tec inference script as default in pipeline

--- debug/scripts/tec_inference.sh
+++ debug/scripts/tec_inference.sh
@@ -56,7 +56,7 @@ fi
 source ~/.bashrc
 conda activate $conda_env
 export PYTHONPATH=
-cmd="python $script_dir/tec_inference.py --obs_num=$obs_num --data_dir=$data_dir --working_dir=$working_dir --ref_dir=$ref_dir --ncpu=$ncpu"
+cmd="python $script_dir/tec_inference_improved.py --obs_num=$obs_num --data_dir=$data_dir --working_dir=$working_dir --ref_dir=$ref_dir --ncpu=$ncpu"
 echo $cmd
 eval $cmd
 EC=$?
