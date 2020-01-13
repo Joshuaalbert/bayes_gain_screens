f578e6b8a312a26fe899630295f789df1cb926ba
Joshua Albert <albert@strw.leidenuniv.nl>
2020-01-13 02:22:51 +0100

    only rough flag for learning

--- bayes_gain_screens/outlier_detection.py
+++ bayes_gain_screens/outlier_detection.py
@@ -6,8 +6,14 @@ from scipy.ndimage import median_filter
 from scipy.spatial import cKDTree
 from bayes_gain_screens.misc import make_coord_array, rolling_window
 from bayes_gain_screens import logging
+from bayes_gain_screens.misc import voronoi_finite_polygons_2d
 from dask.multiprocessing import get
+import pylab as plt
 from timeit import default_timer
+from scipy.spatial import Voronoi
+
+import tensorflow as tf
+from bayes_gain_screens.plotting import plot_vornoi_map
 
 def filter_const(y, init_y_uncert):
     def wrap(p):
@@ -146,48 +152,49 @@ def filter_tec_dir(y,  directions, init_y_uncert=None, min_res=8., **kwargs):
     time_flag = np.tile(np.abs(np.mean(y, axis=-1 ,keepdims=True)) > 16., [1, 1, y.shape[-1]])
     logging.info("Found [{}/{}] banding-outliers (temporal mean DDTEC > 16 mTECU)".format(time_flag.sum(), time_flag.size))
     # # jumps in tec in one time step should be less than a banding (~55 mTECU)
-    # band_jump = np.abs(np.diff(y, axis=-1)) > 40.
-    # time_flag[...,:-1][band_jump] = True
-    # # not sure if it was the value before of after that was bad
-    # time_flag[...,1:][band_jump] = True
+    band_jump = np.abs(np.diff(y, axis=-1)) > 40.
+    time_flag[...,:-1][band_jump] = True
+    # not sure if it was the value before of after that was bad
+    time_flag[...,1:][band_jump] = True
 
-    # Nd, 2
-    X = (directions - np.mean(directions, axis=0)) / np.std(directions, axis=0)
-    final_flags = np.zeros_like(y, dtype=np.bool)
-    Nd, Na, Nt = y.shape
-    maxiter = Nd
-    for t in range(Nt):
-        for a in range(Na):
-            keep = np.logical_not(time_flag[:,a,t])
-            if t > 0:
-                #initialise with OR of previous flags
-                keep = np.logical_or(keep, np.logical_not(final_flags[:, a, t-1]))
-            if keep.sum() < Nd // 2:
-                keep = np.ones(Nd, dtype=np.bool)
-            for i in range(maxiter):
-                #TODO: test solving S parallel RBF problems (sampling provides uncertainty) with smooth=0
-                # spatially a smoothed-RBF should do a decent job of describing the TEC
-                svm = Rbf(X[keep, 0], X[keep, 1], y[keep, a, t], smooth=0.3, **kwargs)
-                y_star = svm(X[:,0], X[:,1])
-                dy = np.abs(y_star - y[:, a, t])
-                # print(np.median(dy), np.percentile(dy, 95))
-                max_dy = np.max(dy[keep])
-                if max_dy < min_res:
-                    break
-                # print(max_dy)
-                keep = dy < max_dy
-                if keep.sum() < 5:
-                    logging.info("Possible outlier flagging divergence time: {} antenna: {}".format(t, a))
-                    break
-
-            reinout_flag = reinout_filter(X[:,0], X[:,1], y[:, a, t])
-
-            final_flags[:, a, t] = np.logical_or(~keep, reinout_flag)
-            # print('CONF', np.interp(dy[~keep], np.percentile(dy, np.linspace(0., 100, 100)), np.linspace(0., 100, 100)))
-
-            # if keep.sum() < Nd:
-            #     logging.info("Time {} ant {} flagged {}".format(t, a, np.where(final_flags[:, a, t])))
-    final_flags = np.logical_or(final_flags, time_flag)
+    # # Nd, 2
+    # X = (directions - np.mean(directions, axis=0)) / np.std(directions, axis=0)
+    # final_flags = np.zeros_like(y, dtype=np.bool)
+    # Nd, Na, Nt = y.shape
+    # maxiter = Nd
+    # for t in range(Nt):
+    #     for a in range(Na):
+    #         keep = np.logical_not(time_flag[:,a,t])
+    #         if t > 0:
+    #             #initialise with OR of previous flags
+    #             keep = np.logical_or(keep, np.logical_not(final_flags[:, a, t-1]))
+    #         if keep.sum() < Nd // 2:
+    #             keep = np.ones(Nd, dtype=np.bool)
+    #         for i in range(maxiter):
+    #             #TODO: test solving S parallel RBF problems (sampling provides uncertainty) with smooth=0
+    #             # spatially a smoothed-RBF should do a decent job of describing the TEC
+    #             svm = Rbf(X[keep, 0], X[keep, 1], y[keep, a, t], smooth=0.3, function='multiquadric')
+    #             y_star = svm(X[:,0], X[:,1])
+    #             dy = np.abs(y_star - y[:, a, t])
+    #             # print(np.median(dy), np.percentile(dy, 95))
+    #             max_dy = np.max(dy[keep])
+    #             if max_dy < min_res:
+    #                 break
+    #             # print(max_dy)
+    #             keep = dy < max_dy
+    #             if keep.sum() < 5:
+    #                 logging.info("Possible outlier flagging divergence time: {} antenna: {}".format(t, a))
+    #                 break
+    #
+    #         reinout_flag = reinout_filter(X[:,0], X[:,1], y[:, a, t])
+    #
+    #         final_flags[:, a, t] = np.logical_or(~keep, reinout_flag)
+    #         # print('CONF', np.interp(dy[~keep], np.percentile(dy, np.linspace(0., 100, 100)), np.linspace(0., 100, 100)))
+    #
+    #         # if keep.sum() < Nd:
+    #         #     logging.info("Time {} ant {} flagged {}".format(t, a, np.where(final_flags[:, a, t])))
+    # final_flags = np.logical_or(final_flags, time_flag)
+    final_flags = time_flag
     logging.info("Flagged {} from {} ({:.2f}%)".format(final_flags.sum(), final_flags.size, 100. * final_flags.sum() / final_flags.size))
     final_y_uncert = np.where(final_flags, np.inf, init_y_uncert)
     return final_y_uncert, final_flags
@@ -261,6 +268,109 @@ def sequential_filter_tec_dir_time(Nd, Nt, X, a, block_size, kwargs, maxiter, sm
                   "flagged", final_flags[:, t:t + block_size].sum(), 'from', Nd * block_size,
                   "({})".format(final_flags[:, t:t + block_size].sum()/(Nd*block_size)))
     return final_flags
+
+def allbutone_flagger(x, y, y_std, epsilon, max_to_flag):
+    '''
+    Iteratively compute the exact rbf interpolant for all data, excluding a point, and compute the missing point.
+    If the difference is larger than the boradcasted y_std, flag it.
+
+    :param x: [N]
+    :param y: [B,N]
+    :param y_std: [B,N]
+    :param epsilon: float
+    :return: [B,N] np.inf where flagged, else y_std
+    '''
+    
+    with tf.Session(graph=tf.Graph()) as sess:
+        x_pl = tf.placeholder(tf.float64, x.shape)
+        y_pl = tf.placeholder(tf.float64, y.shape)
+        y_std_pl = tf.placeholder(tf.float64, y_std.shape)
+        
+        def multiquadric(x1,  x2):
+            r2 = tf.math.squared_difference(x1[:, None]/epsilon,x2[None,:]/epsilon)
+            return tf.math.sqrt(r2 + 1.)
+        def Ab(A):
+            m = tf.gather
+        # N N
+        A = multiquadric(x_pl, x_pl)
+        N = tf.shape(A)[0]
+    
+        def cond(i, flags):
+            # B
+            done = tf.reduce_all(flags, axis=1)
+            return tf.logical_and(i < max_to_flag, tf.reduce_any(tf.logical_not(done)))
+    
+        def body(i, cur_flags):
+            """
+            Predict the residual under all permutations of leave-one-out.
+    
+            :param i: int
+            :param cur_done: tf.bool [B]
+            :param cur_flags: tf.bool [B,N]
+            :return:
+            """
+            def py_build_A(A,y, cur_flags):
+                """
+                Build Ab
+                :param A: [B, N, N]
+                :param y: [B, N]
+                :param cur_flags: [B, N]
+                :return:
+                """
+                y = y.numpy()
+                A = A.numpy()
+                cur_flags = cur_flags.numpy()
+                ind = np.where(cur_flags)
+    
+                #B, N
+                N = y.shape[1]
+                y[cur_flags] = 0.
+                yb = np.tile(y[None, :, :], [N, 1, 1])
+                A[ind[0],ind[1],:] = 0.
+                A[ind[0],:,ind[1]] = 0.
+                A[ind[0],ind[1],ind[1]] = 1.
+                #N B N N
+                Ab = np.tile(A[None, :, :, :], [N, 1, 1, 1])
+                #N, B, 1, N
+                B = np.stack([A[:,i, :] for i in range(N)], axis=0)
+    
+                for i in range(N):
+                    yb[i,:,i] = 0.
+                    Ab[i,:,i,:] = 0.
+                    Ab[i,:,:,i] = 0.
+                    Ab[i,:,i,i] = 1.
+                return [Ab,yb, B]
+    
+            #N B N N, N B N, N B 1 N
+            Ab, yb, B = tf.py_function(py_build_A,[A,y_pl,cur_flags],[y_pl.dtype, y_pl.dtype, y_pl.dtype])
+            #N B N 1
+            wb = tf.linalg.lstsq(Ab, yb[..., None])
+            #N B 1 1
+            y_pred = tf.linalg.matmul(B,wb)
+            #N B N
+            residual = tf.math.abs(y_pl - y_pred[:,:,:,0])
+            #N B N
+            residual = residual*tf.cast(tf.logical_not(cur_flags), residual.dtype)
+            #B N
+            max_res = tf.reduce_max(residual, axis=0)
+            #B N
+            next_flag = max_res > y_std_pl
+            #B
+            # done = tf.reduce_all(next_flag, axis=1)
+            return [i, next_flag]
+    
+        _, _, flags = tf.while_loop(cond,
+                                    body,
+                                    [tf.constant(0),
+                                     tf.zeros(tf.shape(y_pl)[0:1], tf.bool),
+                                     tf.zeros(tf.shape(y_pl), tf.bool)])
+    
+        flags = tf.where(flags, np.inf*tf.ones_like(y_std_pl), y_std_pl)
+
+        return sess.run(flags, {x_pl : x, y_pl: y, y_std_pl:y_std})
+
+
+
 
 
 if __name__ == '__main__':
