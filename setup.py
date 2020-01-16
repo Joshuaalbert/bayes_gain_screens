20c1d0d81c83382d543b13f05408d08c33fa6a7b
Joshua Albert <albert@strw.leidenuniv.nl>
2020-01-16 19:05:11 +0100

    include moddels

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup
 from setuptools import find_packages
 
-__minimum_numpy_version__ = '1.10.0'
-__minimum_tensorflow_version__ = '1.14.0'
+__minimum_numpy_version__ = '1.17.0'
+__minimum_tensorflow_version__ = '1.15.0'
 
 setup_requires = ['numpy>=' + __minimum_numpy_version__, 
 'tensorflow>='+__minimum_tensorflow_version__]
@@ -18,7 +18,7 @@ setup(name='bayes_gain_screens',
     tests_require=[
         'pytest>=2.8',
     ],
-    package_data= {'bayes_gain_screens':['arrays/*']},
+    package_data= {'bayes_gain_screens':['arrays/*', 'flagging_models/*']},
    package_dir = {'':'./'},
    packages=find_packages('./')
      )
