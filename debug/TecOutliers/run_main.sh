b595f95219eba0d6ea44c359edd994acc78640fe
Joshua Albert <albert@strw.leidenuniv.nl>
2020-06-04 00:53:13 +0200

    roc

--- debug/TecOutliers/run_main.sh
+++ debug/TecOutliers/run_main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t skin_cancer_run .
-docker run -v $PWD/training:/training -v $PWD/models:/models -v $PWD/data:/data --rm  --gpus all -it tec_outlier_detection --batch_size=1 --epochs=20 --version=1 --crop_size=10
+docker build -t tec_outlier_detection .
+docker run -v $PWD/training:/training -v $PWD/models:/models -v $PWD/outlier_data:/data --rm -it tec_outlier_detection --batch_size=1 --epochs=20 --version=1 --crop_size=10
