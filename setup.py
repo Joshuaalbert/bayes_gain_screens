869bafbdca891baf88cc0fe0d8e3ffb29165c764
joshuaalbert <albert@strw.leidenuniv.nl>
2020-11-27 15:13:39 +0100

    * pip compliance

--- setup.py
+++ setup.py
@@ -3,23 +3,33 @@
 from setuptools import setup
 from setuptools import find_packages
 
-__minimum_numpy_version__ = '1.16.2'
-__minimum_tensorflow_version__ = '1.15.0'
+__minimum_numpy_version__ = '1.19.0'
 
-setup_requires = ['numpy>=' + __minimum_numpy_version__, 
-'tensorflow>='+__minimum_tensorflow_version__]
+setup_requires = ['numpy<' + __minimum_numpy_version__]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='bayes_gain_screens',
       version='0.0.1',
-      description='Bayesian directional TEC model',
-      author=['Josh Albert'],
-      author_email=['albert@strw.leidenuniv.nl'],
-    setup_requires=setup_requires,  
-    tests_require=[
-        'pytest>=2.8',
-    ],
-    package_data= {'bayes_gain_screens':['arrays/*', 'flagging_models/*']},
-   package_dir = {'':'./'},
-   packages=find_packages('./')
-     )
+      description='Bayesian directional TEC modelling for LOFAR HBA',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/joshuaalbert/bayes_gain_screens",
+      author='Joshua G. Albert',
+      author_email='albert@strw.leidenuniv.nl',
+      setup_requires=setup_requires,
+      tests_require=[
+          'pytest>=2.8',
+      ],
+      package_dir={'': './'},
+      packages=find_packages('./'),
+package_data= {'bayes_gain_screens':['arrays/*', 'flagging_models/*']},
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      )
 
