f4a2941cdf1270a1e2cc9f3270394edd5df7a0f5
joshuaalbert <albert@strw.leidenuniv.nl>
2020-12-12 10:38:47 +0100

    * fix

--- bayes_gain_screens/steps/tec_inference_and_smooth.py
+++ bayes_gain_screens/steps/tec_inference_and_smooth.py
@@ -48,7 +48,7 @@ def unconstrained_solve(freqs, key, Y_obs, amp):
     prior_chain = PriorChain() \
         .push(UniformPrior('tec', -300., 300.)) \
         .push(UniformPrior('const', -1.1*jnp.pi, 1.1*jnp.pi)) \
-        .push(UniformPrior('clock', -10., 10.)) \
+        .push(UniformPrior('clock', -1., 1.)) \
         .push(HalfLaplacePrior('uncert', 0.2))
 
     # prior_chain.test_prior(key, 10000, log_likelihood=log_likelihood)
