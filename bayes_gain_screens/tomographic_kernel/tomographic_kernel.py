a77c511c2bcbbb8dfb4cf45b016e378f31b4a648
joshuaalbert <albert@strw.leidenuniv.nl>
2021-06-07 23:27:28 -0400

    tomographic kernel

--- bayes_gain_screens/tomographic_kernel/tomographic_kernel.py
+++ bayes_gain_screens/tomographic_kernel/tomographic_kernel.py
@@ -14,7 +14,8 @@ def scan_vmap(f):
 
     return run
 
-def tec_kernel(x1,k1,x2,k2,x0,bottom, width, l, sigma, S_marg, fed_kernel, *fed_kernel_params):
+
+def tec_kernel(x1, k1, x2, k2, x0, bottom, width, l, sigma, S_marg, fed_kernel, *fed_kernel_params):
     def ray_integral(f, x, k):
         smin = (bottom - (x[2] - x0[2])) / k[2]
         smax = (bottom + width - (x[2] - x0[2])) / k[2]
@@ -29,18 +30,18 @@ def tec_kernel(x1,k1,x2,k2,x0,bottom, width, l, sigma, S_marg, fed_kernel, *fed_
     Ky = lambda x, x2, k2: vmap(lambda x2, k2: ray_integral(lambda y: K(x, y), x2, k2))(*jnp.broadcast_arrays(x2, k2))
     Kxy = lambda x1, k1, x2, k2: vmap(lambda x1, k1: ray_integral(lambda x: Ky(x, x2, k2), x1, k1))(
         *jnp.broadcast_arrays(x1, k1))
-    return Kxy(x1,k1,x2,k2)
+    return Kxy(x1, k1, x2, k2)
 
 
 class TomographicKernel(Kernel):
-    def __init__(self, x0, ref_ant, fed_kernel:StationaryKernel, S_marg=25, compute_tec=False):
+    def __init__(self, x0, ref_ant, fed_kernel: StationaryKernel, S_marg=25, compute_tec=False):
         self.S_marg = S_marg
         self.x0 = x0
         self.ref_ant = ref_ant
         self.fed_kernel = fed_kernel
         self.compute_tec = compute_tec
 
-    def __call__(self, X1, X2, bottom, width, l, sigma, *fed_kernel_params):
+    def __call__(self, X1, X2, bottom, width, l, sigma, *fed_kernel_params, wind_velocity=None):
         x1 = X1[:, 0:3]  # N,3
         k1 = X1[:, 3:6]
         x2 = X2[:, 0:3]  # M,3
@@ -52,15 +53,29 @@ class TomographicKernel(Kernel):
             ds = (smax - smin)
             _x = x + k * smin
             _k = k * ds
-            t = jnp.linspace(0., 1., self.S_marg+1)
-            return jnp.sum(scan_vmap(lambda t: f(_x + t*_k))(t), axis=0) * (ds/self.S_marg)
+            t = jnp.linspace(0., 1., self.S_marg + 1)
+            return jnp.sum(scan_vmap(lambda t: f(_x + t * _k))(t), axis=0) * (ds / self.S_marg)
 
-        K = lambda x, y: self.fed_kernel(x[None], y[None], l, sigma, *fed_kernel_params)[0,0] #* self.fed_kernel(x[None]-h, y[None]-h, width, 1., *fed_kernel_params)[0,0]
-        Ky = lambda x, x2, k2: vmap(lambda x2, k2: ray_integral(lambda y: K(x, y), x2, k2))(*jnp.broadcast_arrays(x2, k2))
-        Kxy = lambda x1, k1, x2, k2: vmap(lambda x1, k1: ray_integral(lambda x: Ky(x, x2, k2), x1, k1))(*jnp.broadcast_arrays(x1,k1))
+        K = lambda x, y: self.fed_kernel(x[None], y[None], l, sigma, *fed_kernel_params)[0, 0]
+        Ky = lambda x, x2, k2: vmap(lambda x2, k2: ray_integral(lambda y: K(x, y), x2, k2))(
+            *jnp.broadcast_arrays(x2, k2))
+        Kxy = lambda x1, k1, x2, k2: vmap(lambda x1, k1: ray_integral(lambda x: Ky(x, x2, k2), x1, k1))(
+            *jnp.broadcast_arrays(x1, k1))
 
         if self.compute_tec:
-            return Kxy(x1,k1,x2,k2)
+            if wind_velocity is not None:
+                t1 = X1[:, 6:7]
+                x1 = x1 - t1 * wind_velocity
+                t2 = X2[:, 6:7]
+                x2 = x2 - t2 * wind_velocity
+                return Kxy(x1, k1, x2, k2)
         else:
-            return Kxy(x1, k1, x2, k2) + Kxy(self.ref_ant, k1, self.ref_ant, k2) \
-                   - Kxy(x1, k1, self.ref_ant, k2) - Kxy(self.ref_ant, k1, x2, k2)
+            if wind_velocity is not None:
+                t1 = X1[:, 6:7]
+                x1 = x1 - t1 * wind_velocity
+                t2 = X2[:, 6:7]
+                x2 = x2 - t2 * wind_velocity
+                return Kxy(x1, k1, x2, k2) + Kxy(self.ref_ant - t1 * wind_velocity, k1,
+                                                 self.ref_ant - t2 * wind_velocity, k2) \
+                       - Kxy(x1, k1, self.ref_ant - t2 * wind_velocity, k2) - Kxy(self.ref_ant - t1 * wind_velocity, k1,
+                                                                                  x2, k2)
